4f25cb2af0a27f2507bc7830d4f68288b3652e8d
HaGiaKiet <hagiakiet23@gmail.com>
2025-06-25 12:50:54 +0700

    Sửa Form thêm liên hệ phiên bản 2

--- ContactList/AddContactForm.Designer.cs
+++ ContactList/AddContactForm.Designer.cs
@@ -31,11 +31,12 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(400, 54);
+            this.textBox1.Location = new System.Drawing.Point(437, 54);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(125, 20);
             this.textBox1.TabIndex = 0;
@@ -43,7 +44,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(383, 103);
+            this.button1.Location = new System.Drawing.Point(78, 36);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(108, 54);
             this.button1.TabIndex = 1;
@@ -52,17 +53,27 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(345, 176);
+            this.textBox2.Location = new System.Drawing.Point(262, 214);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(318, 20);
             this.textBox2.TabIndex = 2;
             this.textBox2.Text = "Email";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(381, 262);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 8);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // AddContactForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
@@ -78,5 +89,6 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
     }
 }
